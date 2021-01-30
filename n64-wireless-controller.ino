4507408ec2318225a3501a2df0fcdc33df3d39f5
Roger Fernandes <roger.osbourne@gmail.com>
2021-01-30 03:23:18 -0300

    first test

--- n64-wireless-controller.ino
+++ n64-wireless-controller.ino
@@ -1,15 +1,61 @@
 #include <N64Controller.h>
+#include <SPI.h>
+#include "printf.h"
+#include "RF24.h"
 
-N64Controller player1 (12);
+#define CE_PIN 7
+#define CSN_PIN 8
+#define CTRL_PIN 5
+
+struct RfDataStruct
+{
+  bool btA = false;
+  bool btB = false;
+};
+
+typedef struct RfDataStruct rfDataStruct;
+rfDataStruct rfData;
+
+const uint8_t pipes[][6] = {
+  "oi"
+};
+
+RF24 radio(CE_PIN, CSN_PIN);
+N64Controller ctrl (CTRL_PIN);
 
 void setup() {
-    player1.begin();
-    Serial.begin(115200);
+  Serial.begin(115200);
+  while (!Serial) {}
+  
+  ctrl.begin();
+  
+  if (!radio.begin()) {
+    Serial.println(F("radio hardware is not responding!!"));
+    while (1) {}
+  }
+  radio.setPALevel(RF24_PA_LOW);
+  radio.setPayloadSize(sizeof(rfData));
+  radio.openWritingPipe(pipes[0]);
+  radio.stopListening();
+
+  printf_begin();             // needed only once for printing details
+  radio.printDetails();       // (smaller) function that prints raw register values
+  radio.printPrettyDetails(); // (larger) function that prints human readable data
+  delay(5000);
 }
 
 void loop() {
-    delay(5000);
-    player1.update();
-    Serial.println("##########");
-    player1.print_N64_status();
+  ctrl.update();
+  rfData.btA = ctrl.A();
+  rfData.btB = ctrl.B();
+
+  Serial.print("BT_A = ");
+  Serial.print(rfData.btA);
+  Serial.print(", BT_B = ");
+  Serial.print(rfData.btB);
+  Serial.println(" ");
+
+  radio.write(&rfData, sizeof(rfData));
+
+  delay(1000);
 }
